2f22445c6016b43514d998335e889fa847cfedf7
rebeccaBull <max.liu@robo-sys.com>
2017-08-18 21:44:22 +0800

    初始化项目

---
+++ 妙汇_电商_Swift/妙汇_电商_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  妙汇_电商_Swift
+//
+//  Created by 刘渊 on 2017/8/18.
+//  Copyright © 2017年 刘渊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
